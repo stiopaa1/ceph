c5317856734f754c637aeb1d81f0b40bd1371a0b
Samuel Just <sjust@redhat.com>
2014-11-18 17:59:36 -0800

    osd/: don't block writes on degraded objects
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -1418,12 +1418,6 @@ void ReplicatedPG::do_op(OpRequestRef& op)
     return;
   }
 
-  // degraded object?
-  if (write_ordered && is_degraded_object(head)) {
-    wait_for_degraded_object(head, op);
-    return;
-  }
-
   // missing snapdir?
   hobject_t snapdir(m->get_oid(), m->get_object_locator().key,
 		    CEPH_SNAPDIR, m->get_pg().ps(), info.pgid.pool(),
@@ -7992,9 +7986,6 @@ void ReplicatedBackend::sub_op_modify(OpRequestRef op)
   // sanity checks
   assert(m->map_epoch >= get_info().history.same_interval_since);
   
-  // we better not be missing this.
-  assert(!parent->get_log().get_missing().is_missing(soid));
-
   int ackerosd = m->get_source().num();
   
   op->mark_started();
